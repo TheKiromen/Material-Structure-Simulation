cae5cfa47b07b5f9a5e2e24aa06a089646985971
Dominik Kruczek <TheKiromen@gmail.com>
2023-01-01 18:35:32 +0100

    Finished GUI

--- mainApp.py
+++ mainApp.py
@@ -1,19 +1,9 @@
 import os
-import time
 from threading import Thread
 from generateMesh import generateMesh
 from generateMicrostructure import generate_microstructure
 import PySimpleGUI as gui
 
-# Simulation variables
-algorithm = "CA"
-from_empty_simulation = True
-step_limit = 50
-absorbing = False
-random_nucleation_sites = True
-neighbourhood_type = "VN"
-number_of_grain_types = 5
-number_of_nucleation_sites = 100
 
 # Set app theme
 gui.theme('dark grey 11')
@@ -24,6 +14,57 @@ def popup(message):
     gui.theme('Black')
     content = [[gui.Text(message)]]
     return gui.Window('Message', content, no_titlebar=True, keep_on_top=True, finalize=True)
+
+
+def meshGeneration():
+    # Lock the window
+    window.disable()
+    # Generate the mesh
+    generateMesh()
+    # Change the images
+    window['mesh_img'].update(filename='output/mesh.png')
+    # Finish thread execution
+    window.write_event_value('Finished', "Mesh")
+
+
+def microstructureGeneration():
+    # Simulation type
+    if values['sim_type'] == "Monte Carlo":
+        algorithm = "MC"
+    else:
+        algorithm = "CA"
+    # Random seeding
+    random_nucleation_sites = not values["random_seeding"]
+    # Simulation boundary
+    absorbing = values['absorbing']
+    # Neighbourhood type
+    if values['neighbour_type'] == "Random Hexagonal":
+        neighbourhood_type = "Hex"
+    else:
+        neighbourhood_type = "VN"
+    # Create from empty simulation
+    from_empty_simulation = values['empty_sim']
+    # Amount of seeds
+    number_of_nucleation_sites = int(values['seeds'])
+    # Amount of grain types
+    number_of_grain_types = int(values['grains'])
+    # Steps limit
+    step_limit = int(values['steps'])
+
+    # Lock the window
+    window.disable()
+
+    # Generate the Microstructure
+    generate_microstructure(algorithm, random_nucleation_sites, absorbing, neighbourhood_type,
+                            from_empty_simulation, number_of_nucleation_sites, number_of_grain_types, step_limit)
+
+    # Change images
+    window['input_img'].update(filename='output/Input.png')
+    window['output_img'].update(filename='output/Output.png')
+    window['mesh_button'].update(disabled=False)
+    
+    # Finish thread execution
+    window.write_event_value('Finished', "Microstructure")
 
 
 # Check for existing data files
@@ -63,9 +104,9 @@ layout = [
     )],
     [gui.Checkbox("Create from empty simulation", default=True, disabled=True, key="empty_sim")],
     [gui.Text("Steps limit ", pad=((0, 0), (20, 0))),
-     gui.Slider((10, 150), 50, 1, orientation="horizontal")],
-    [gui.Checkbox("Absorbing boundary ")],
-    [gui.Checkbox("Periodic seeding ")],
+     gui.Slider((10, 150), 50, 1, orientation="horizontal", key="steps")],
+    [gui.Checkbox("Absorbing boundary ", key="absorbing")],
+    [gui.Checkbox("Periodic seeding ", key="random_seeding")],
     [gui.Text("Neighbourhood type "), gui.Combo(
         ["Von Neumann", "Random Hexagonal"],
         default_value="Von Neumann",
@@ -74,9 +115,9 @@ layout = [
         enable_events=True
     )],
     [gui.Text("Number of grain types ", pad=((0, 0), (20, 0))),
-     gui.Slider((2, 9), 5, 1, orientation="horizontal")],
+     gui.Slider((2, 9), 5, 1, orientation="horizontal", key="grains")],
     [gui.Text("Number of seeds ", pad=((0, 0), (20, 0))),
-     gui.Slider((10, 250), 100, 1, orientation="horizontal")],
+     gui.Slider((10, 250), 100, 1, orientation="horizontal", key="seeds")],
 
     # Buttons
     [gui.HSeparator(pad=(0, 20))],
@@ -98,25 +139,21 @@ while True:
         break
     # Generate microstructure
     elif event == 'microstructure_button':
-        
         tmp = popup("Processing...")
-        generate_microstructure(algorithm, random_nucleation_sites, absorbing, neighbourhood_type,
-                                from_empty_simulation, number_of_nucleation_sites, number_of_grain_types, step_limit)
-        window['input_img'].update(filename='output/Input.png')
-        window['output_img'].update(filename='output/Output.png')
-        window['mesh_button'].update(disabled=False)
-        # tmp.close()
+        T = Thread(target=microstructureGeneration).start()
     # Generate mesh from microstructure
     elif event == 'mesh_button':
         tmp = popup("Processing...")
-        generateMesh()
-        window['mesh_img'].update(filename='output/mesh.png')
+        T = Thread(target=meshGeneration).start()
     # Change options visibility
     elif event == 'sim_type':
         if values['sim_type'] == "Monte Carlo":
             window['empty_sim'].update(disabled=False)
         elif values['sim_type'] == "Cellular Automata":
             window['empty_sim'].update(disabled=True)
+    elif event == 'Finished':
+        window.enable()
+        tmp.close()
 
 # Finish up by removing from the screen
 window.close()
