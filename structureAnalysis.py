50bb9800e7e72624efffee056614aa5fb6d553b0
Dominik Kruczek <TheKiromen@gmail.com>
2023-01-05 14:51:57 +0100

    Contour extraction for all grains

--- structureAnalysis.py
+++ structureAnalysis.py
@@ -5,25 +5,39 @@ import numpy as np
 # First one is black, it signifies no grain at all
 colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255),
           (255, 0, 255), (125, 125, 255), (125, 255, 125), (255, 125, 125)]
+global_contours = []
+global_hierarchy = []
 
 # Load the image
 image = cv.imread(r'output/Output.png')
 
-# Create color mask
-mask = cv.inRange(image, colors[1], colors[1])
-# Apply the mask
-masked_img = cv.bitwise_and(image, image, mask=mask)
+for i in range(1, 10):
+    # Create color mask
+    mask = cv.inRange(image, colors[i], colors[i])
+    # Apply the mask
+    masked_img = cv.bitwise_and(image, image, mask=mask)
 
-# Convert image to gray
-grayscale = cv.cvtColor(masked_img, cv.COLOR_BGR2GRAY)
-# Bin the image
-flag, binned = cv.threshold(grayscale, 10, 255, cv.THRESH_BINARY)
-# Find contours
-contours, hierarchy = cv.findContours(binned, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# Draw the contours
-result = np.zeros(image.shape)
-cv.drawContours(result, contours, -1, (255, 255, 255), 1)
+    # Convert image to gray
+    grayscale = cv.cvtColor(masked_img, cv.COLOR_BGR2GRAY)
+    # Bin the image
+    flag, binned = cv.threshold(grayscale, 10, 255, cv.THRESH_BINARY)
+    # Find contours from single channel
+    color_contours, hierarchy = cv.findContours(binned, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # Save found contours
+    global_contours.append(color_contours)
+    global_hierarchy.append(hierarchy)
 
-# Display the result
-cv.imshow("Image", result)
+    # Display single colored grains
+    cv.imshow("Image", masked_img)
+    cv.waitKey(0)
+
+# # Create empty image
+# result = np.zeros(image.shape)
+# # Draw all contours onto the image
+# for contour in global_contours:
+#     cv.drawContours(result, contour, -1, (255, 255, 255), 1)
+# # Display the result
+# cv.imshow("All contours", result)
+
 cv.waitKey(0)
+cv.destroyAllWindows()
